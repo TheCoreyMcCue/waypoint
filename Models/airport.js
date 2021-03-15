819335b10eb7fe37f22fc0f9b569265d58524a53
Corey McCue <coreysmccue@gmail.com>
2021-03-15 13:21:34 +0100

    add tiedown and landing fee to airport model and adjust 'new' form

--- Models/airport.js
+++ Models/airport.js
@@ -5,7 +5,10 @@ const AirportSchema = new Schema({
     name: String,
     image: String,
     description: String,
-    location: String
+    location: String,
+    icao: String,
+    landingFee: Number,
+    tieDown: Number
 });
 
 module.exports = mongoose.model('Airport', AirportSchema);
