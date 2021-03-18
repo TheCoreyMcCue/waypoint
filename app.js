ea10cb1f3bdb6330665f2df2adfd95e0d221cf43
Corey McCue <coreysmccue@gmail.com>
2021-03-18 14:13:00 +0100

    add bootstrap error alert

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ app.all('*', (req, res, next) => {
 
 app.use((err, req, res, next) => {
     const {statusCode = 500, message = 'Clearance denied'} = err;
-    res.status(statusCode).send(message);
+    res.status(statusCode).render('error');
     res.send('no fly zone')
 });
 
