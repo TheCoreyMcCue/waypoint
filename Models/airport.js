be9659d0f3aca6f16ebc627113f511ae24620201
Corey McCue <coreysmccue@gmail.com>
2021-03-19 12:41:05 +0100

    implement middleware for dependant destroy for reviews connected to an airport

--- Models/airport.js
+++ Models/airport.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Review = require('./review');
 const Schema = mongoose.Schema;
 
 const AirportSchema = new Schema({
@@ -16,5 +17,15 @@ const AirportSchema = new Schema({
         }
     ]
 });
+
+AirportSchema.post('findOneAndDelete', async function(doc){
+    if(doc){
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
+})
 
 module.exports = mongoose.model('Airport', AirportSchema);
