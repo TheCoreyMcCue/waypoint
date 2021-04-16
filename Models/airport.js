cbf9994d89076c7ccf0852a44c3fd26a29414a8e
Corey McCue <coreysmccue@gmail.com>
2021-04-16 10:11:28 +0200

    convert edit page photos to thumbnails

--- Models/airport.js
+++ Models/airport.js
@@ -3,13 +3,16 @@ const mongoose = require('mongoose');
 const Review = require('./review');
 const Schema = mongoose.Schema;
 
+const ImageSchema = new Schema({
+     url: String,
+        filename: String
+})
+ImageSchema.virtual('thumbnail').get(function() {
+    return this.url.replace('/upload', '/upload/w_200');
+})
 const AirportSchema = new Schema({
     name: String,
-    images: [
-        { url: String,
-          filename: String
-        }
-    ],
+    images: [ImageSchema],
     description: String,
     location: String,
     icao: String,
