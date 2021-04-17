8952acda2b391a110dfa5657f2fcb847906cc567
Corey McCue <coreysmccue@gmail.com>
2021-04-17 09:18:47 +0200

    make map feature dynamic, but still need to fix seeds

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,7 +1,13 @@
+// const airport = require("../../models/airport");
+
 mapboxgl.accessToken = mapToken; 
 const map = new mapboxgl.Map({
     container: 'map', // container ID
     style: 'mapbox://styles/mapbox/streets-v11', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 5 // starting zoom
+    center: airport.geometry.coordinates, // starting position [lng, lat]
+    zoom: 8 // starting zoom
 });
+
+new mapboxgl.Marker()
+.setLngLat(airport.geometry.coordinates)
+.addTo(map)
