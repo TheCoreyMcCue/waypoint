c3ea77a2a72734fdf0fc861b1ad0677f8c960a07
Corey McCue <coreysmccue@gmail.com>
2021-04-20 09:47:59 +0200

    install mongo sanitize and sanitize html to protect aagainst injections and xss

--- schemas.js
+++ schemas.js
@@ -1,21 +1,45 @@
-const Joi = require('joi');
+const { sanitize } = require('express-mongo-sanitize');
+const BaseJoi = require('joi');
+const sanitizeHtml = require('sanitize-html');
+
+const extension = (joi) => ({
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML': '{{#label}} must not include HTML!'
+    },
+    rules: {
+        escapeHTML: {
+            validate(value, helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags: [],
+                    allowedAttributes: {},
+                });
+                if (clean !== value) return helpers.error('string.escapeHTML', { value })
+                return clean;
+            }
+        }
+    }
+});
+
+const Joi = BaseJoi.extend(extension);
 
 module.exports.airportSchema = Joi.object({
     airport: Joi.object({
-        name: Joi.string().required(),
+        name: Joi.string().required().escapeHTML(),
         landingFee: Joi.number().required().min(0),
         tieDown: Joi.number().required().min(0),
-        icao: Joi.string().required(),
+        icao: Joi.string().required().escapeHTML(),
         // image: Joi.string().required(),
-        location: Joi.string().required(),
-        description: Joi.string().required()
+        location: Joi.string().required().escapeHTML(),
+        description: Joi.string().required().escapeHTML()
     }).required(),
     deleteImages: Joi.array()
 });
 
 module.exports.reviewSchema = Joi.object({
     review: Joi.object({
-        body: Joi.string().required(),
+        body: Joi.string().required().escapeHTML(),
         rating: Joi.number().required().min(1).max(5)
     }).required()
 });
